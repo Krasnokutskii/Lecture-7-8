25233ed6b44b65cbdac5e6ab80179fb5d6d10cdc
Iaroslav Krasnokutskii <Yroslav1995@>
2020-08-18 18:35:39 +0300

    Initial Commit

---
+++ Lecture 7 8/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Lecture 7 8
+//
+//  Created by Ярослав on 8/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lecture 7 8/Lecture_7_8App.swift
@@ -0,0 +1,17 @@
+//
+//  Lecture_7_8App.swift
+//  Lecture 7 8
+//
+//  Created by Ярослав on 8/18/20.
+//
+
+import SwiftUI
+
+@main
+struct Lecture_7_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
